6755c4e23d0a1c1a310e174139cd819a26ebb7f3
rakesh <rakeshsagartfi21@gmail.com>
2025-06-14 10:40:59 +0000

    Added CSS

--- src/components/LettersCalculator/index.js
+++ src/components/LettersCalculator/index.js
@@ -7,23 +7,28 @@ import './index.css'
 class LettersCalculator extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <div>
-            <h1> Calculate the Letters you enter </h1>
-            <div>
-              <label htmlFor="phraseText"> Enter the phrase </label>
+      <div className="app-container">
+        <div className="letters-calculator-container">
+          <div className="calculator-container">
+            <h1 className="heading"> Calculate the Letters you enter </h1>
+            <div className="phrase-input-container">
+              <label htmlFor="phraseText" className="label">
+                {' '}
+                Enter the phrase{' '}
+              </label>
               <input
                 type="input"
                 id="phraseText"
                 placeholder="Enter the phrase"
+                className="phrase-input"
               />
             </div>
-            <p> No.of letters: 0</p>
+            <p className="letters-count"> No.of letters: 0</p>
           </div>
           <img
             src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png"
             alt="letters calculator"
+            className="letters-calculator-image"
           />
         </div>
       </div>
